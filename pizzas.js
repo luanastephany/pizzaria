7f54008dfba3db853532482e436a04ab908b6023
Luana <ls.luanastephany@gmail.com>
2021-05-01 16:21:35 -0300

    script finalizado

--- pizzas.js
+++ pizzas.js
@@ -1,9 +1,9 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+    { id: 1, name: 'Mussarela', img: 'images/pizza.png', price: 25.50, sizes: ['100g', '530g', '860g'], description: 'Pizza clássica de mussarela com pedaços opcionais de azeitonas pretas' },
+    { id: 2, name: 'Molho de tomate', img: 'images/pizza2.png', price: 20.00, sizes: ['320g', '530g', '860g'], description: 'Pizza de molho de tomate com queijo parmesão' },
+    { id: 3, name: 'Tomate com azeitona', img: 'images/pizza3.png', price: 25.99, sizes: ['320g', '530g', '860g'], description: 'Pizza de tomate com azeitonas pretas' },
+    { id: 4, name: 'Brocolis', img: 'images/pizza4.png', price: 20.50, sizes: ['320g', '530g', '860g'], description: 'Pizza de brocolis recheada com queijo em abundância e fatias de tomate' },
+    { id: 5, name: 'Pimentão', img: 'images/pizza5.png', price: 20.99, sizes: ['320g', '530g', '860g'], description: 'Pizza de queijo mussarela com pedaços de pimentão' },
+    { id: 6, name: 'Moda da Casa', img: 'images/pizza6.png', price: 25.99, sizes: ['320g', '530g', '860g'], description: 'Pizza chefe da casa, com queijo provolone, fatias de tomate e azeitona' },
+    { id: 7, name: 'Chocolate', img: 'images/pizza7.png', price: 30.50, sizes: ['320g', '530g', '860g'], description: 'Pizza de chocolate com grandes pedaços de MMs coloridos espalhados' }
 ];
